4276c446cf74e8a0ed8769af2513b2d380dfa330
Vaibhav Bhosale <106335135+MrVaibhavBhosale@users.noreply.github.com>
2026-02-06 20:49:31 +0530

    Add TEMPLATES setting for Django
    Added TEMPLATES configuration for Django templates.

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,27 @@ CORS_ALLOW_HEADERS = [
     "content-type",
 ]
 
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+
+        # 👇 HA IMPORTANT
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
+
+        "APP_DIRS": True,
+
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+
 # ================= EMAIL & SMS =================
 
 SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
