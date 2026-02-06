0982044f2f402e72bb9fb1e9f6e4ccfda28a007c
Vaibhav Bhosale <106335135+MrVaibhavBhosale@users.noreply.github.com>
2026-02-06 21:25:25 +0530

    Refactor permission_classes syntax and update Swagger path

--- config/urls.py
+++ config/urls.py
@@ -26,7 +26,7 @@ schema_view = get_schema_view(
         license=openapi.License(name="BSD License"),
     ),
     public=True,
-    permission_classes=(permissions.AllowAny,),
+    permission_classes=[permissions.AllowAny],
 )
 
 urlpatterns = [
@@ -40,11 +40,7 @@ urlpatterns = [
     ),
 
     # Swagger UI at root
-    path(
-        "",
-        schema_view.with_ui("swagger", cache_timeout=0),
-        name="schema-swagger-ui",
-    ),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
 
     # JWT
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
